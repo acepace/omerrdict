510fffa0c763d1e34353b9a609b515fdc3e0950c
acepace <acepace@gmail.com>
2014-05-09 20:16:04 +0300

    Removed hello world

--- endpointsController.py
+++ endpointsController.py
@@ -1,7 +1,6 @@
 import endpoints
 
 from rpc import wordAPI
-from rpc import helloworld_api
 package = 'omerrdict'
 
-APPLICATION = endpoints.api_server([wordAPI.WordAPIApi,helloworld_api.HelloWorldApi])
+APPLICATION = endpoints.api_server([wordAPI.WordAPIApi])
